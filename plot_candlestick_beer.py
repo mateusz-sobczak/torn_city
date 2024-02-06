c372c20103e68f58f95a6f08f155888afc1a5b56
Mateusz Sobczak <mateusz.sobczak@tcs.com>
2024-02-06 01:13:54 +0000

    Added scal to 0, and line for selling polit

--- plot_candlestick_beer.py
+++ plot_candlestick_beer.py
@@ -4,9 +4,12 @@ import pandas as pd
 df = pd.read_csv('datasets/beer.csv')
 fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_ItemMarket'].str.replace('$', '').astype(int), name='Item Market'),
-    go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].str.replace('$', '').astype(int), name='Bazaar')
+    go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].str.replace('$', '').astype(int), name='Bazaar'),
 ])
 
+sell = 885
+fig.add_hline(y=sell, line_dash='dash', line_color='green', annotation_text=f"Selling Point {sell}")
+fig.update_yaxes(rangemode="tozero")
 fig.update_layout(title='Bottle of Beer')
 fig.show()
 
@@ -16,6 +19,9 @@ fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].str.replace('$', '').astype(int), name='Bazaar')
 ])
 
+sell = 900
+fig.add_hline(y=sell, line_dash='dash', line_color='green', annotation_text=f"Selling Point {sell}")
+fig.update_yaxes(rangemode="tozero")
 fig.update_layout(title='Bottle of Sake')
 fig.show()
 
@@ -25,5 +31,8 @@ fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].str.replace('$', '').astype(int), name='Bazaar')
 ])
 
+sell = 900
+fig.add_hline(y=sell, line_dash='dash', line_color='green', annotation_text=f"Selling Point {sell}")
+fig.update_yaxes(rangemode="tozero")
 fig.update_layout(title='Bottle of Tequila')
 fig.show()
