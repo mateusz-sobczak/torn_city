56844b81e1bbb7e8fb260292d555b8dd9dc0c193
Mateusz Sobczak <mateusz.sobczak@tcs.com>
2024-02-07 23:05:59 +0000

    Renaming of files + add for small first aid kit

--- plot_line_beer_sake_tequila.py
+++ plot_line_beer_sake_tequila.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
 
-df = pd.read_csv('tickers/datasets/tick_beer.csv')
+df = pd.read_csv('tickers/datasets/tick_bottle_of_beer.csv')
 fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_ItemMarket'].astype(int), name='Item Market'),
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].astype(int), name='Bazaar'),
@@ -13,7 +13,7 @@ fig.update_yaxes(rangemode="tozero")
 fig.update_layout(title='Bottle of Beer')
 fig.show()
 
-df = pd.read_csv('tickers/datasets/tick_sake.csv')
+df = pd.read_csv('tickers/datasets/tick_bottle_of_sake.csv')
 fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_ItemMarket'].astype(int), name='Item Market'),
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].astype(int), name='Bazaar')
@@ -25,7 +25,7 @@ fig.update_yaxes(rangemode="tozero")
 fig.update_layout(title='Bottle of Sake')
 fig.show()
 
-df = pd.read_csv('tickers/datasets/tick_tequila.csv')
+df = pd.read_csv('tickers/datasets/tick_bottle_of_tequila.csv')
 fig = go.Figure([
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_ItemMarket'].astype(int), name='Item Market'),
     go.Scatter(x=pd.to_datetime(df['Tick'], unit='s'), y=df['Price_Bazaar'].astype(int), name='Bazaar')
---
+++ tickers/small_first_aid_kit_indicator.py
@@ -0,0 +1,14 @@
+import market
+import time
+
+key = open('../key.txt').readline()
+market = market.Market(key=key)
+
+item = market.get_item_id(item='Small First Aid Kit')
+
+while True:
+    with open(file='datasets/tick_small_first_aid_kit.csv', mode='a') as file:
+        item_market, bazaar = market.get_items(item=item, market='itemmarket,bazaar')
+        file.write(f'{round(time.time())},{item_market[0]["cost"]},{bazaar[0]["cost"]}\n')
+        file.close()
+    time.sleep(10)
