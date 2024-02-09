50ccf913a00f28f5f793362f8debd13fa37ae7c5
Mateusz Sobczak <mateusz.sobczak@tcs.com>
2024-02-09 21:18:55 +0000

    Added extra tickers + try except statement testing

--- tickers/can_of_goose_juice_indicator.py
+++ tickers/can_of_goose_juice_indicator.py
@@ -4,10 +4,10 @@ import time
 key = open('../key.txt').readline()
 market = market.Market(key=key)
 
-item = market.get_item_id(item='Bottle of Beer')
+item = market.get_item_id(item='Can of Goose Juice')
 
 while True:
-    with open(file='datasets/tick_bottle_of_beer.csv', mode='a') as file:
+    with open(file='datasets/tick_can_of_goose_juice.csv', mode='a') as file:
         try:
             item_market, bazaar = market.get_items(item=item, market='itemmarket,bazaar')
             file.write(f'{round(time.time())},{item_market[0]["cost"]},{bazaar[0]["cost"]}\n')
