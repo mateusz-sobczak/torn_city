914d1424e75c98c0b3ba6401f5f454d7c57a25e6
Mateusz Sobczak <sobczakmateusz@outlook.com>
2024-02-09 20:53:01 +0000

    Add try except to all tickers

--- tickers/bottle_of_sake_indicator.py
+++ tickers/bottle_of_sake_indicator.py
@@ -8,7 +8,11 @@ item = market.get_item_id(item='Bottle of Sake')
 
 while True:
     with open(file='datasets/tick_bottle_of_sake.csv', mode='a') as file:
-        item_market, bazaar = market.get_items(item=item, market='itemmarket,bazaar')
-        file.write(f'{round(time.time())},{item_market[0]["cost"]},{bazaar[0]["cost"]}\n')
-        file.close()
+        try:
+            item_market, bazaar = market.get_items(item=item, market='itemmarket,bazaar')
+            file.write(f'{round(time.time())},{item_market[0]["cost"]},{bazaar[0]["cost"]}\n')
+            file.close()
+        except Exception as err:
+            print(err)
+            continue
     time.sleep(10)
